2d1a036c2237b021cd0e8584dc92412d99651f44
wahek <wahek1999@mail.ru>
2024-02-18 12:23:51 +0300

    issue absorbed

--- e_com/main_models_2/models.py
+++ e_com/main_models_2/models.py
@@ -40,7 +40,7 @@ class Item(models.Model):
 
 
 class Order(models.Model):
-    id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    id_item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    id_client = models.ManyToManyField(Client)
+    id_item = models.ManyToManyField(Item)
     total_sum = models.IntegerField()
     date_of_creation = models.DateTimeField(auto_now_add=True)
